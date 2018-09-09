1fe940d2cf26db5077e197ee0e021d2f530fed57
tritoke <samuelleonard35@googlemail.com>
2018-09-09 20:21:43 +0100

    Deleted excess files from the repo.

--- level-1.py
+++
@@ -1,8 +0,0 @@
-import base64
-hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-answer = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-arr = []
-for i in range(0, len(hex_string), 2):
-    arr.append(int(hex_string[i] + hex_string[i+1], 16))
-
-print(base64.b64encode(bytes(arr)) == answer)
